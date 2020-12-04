a2cb3bd8e11136eb83a620fbfdbf062055b73c96
Katsuya Kikuchi <k.kikuchi.ah@gmail.com>
2020-12-04 22:42:00 +0900

    class study

--- class.rb
+++ class.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
-# test class
-class TEST
-  def call(name)
-    puts name
-  end
+class User
+  attr_accessor :name
+  attr_reader :address
 end
-
-t = TEST.new
-# メソッドは括弧がなくても呼べるらしい
-t.call 'hello'
---
+++ test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require './class'
+
+user = User.new
+user.name = "test"
+puts user.name
+puts user.address
+
