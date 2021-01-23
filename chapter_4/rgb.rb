4c8175ddac8988718c425c6cacaad3f17af580c4
Katsuya Kikuchi <k.kikuchi.ah@gmail.com>
2021-01-24 01:50:56 +0900

    add rgb and test

--- chapter_4/rgb.rb
+++ chapter_4/rgb.rb
@@ -1,3 +1,10 @@
 def to_hex(r, g, b)
-  '#000000'
+  [r, g, b].inject('#') { |hex, value| hex += value.to_s(16).rjust(2, '0') }
+end
+
+def to_ints(hex)
+  r = hex[1..2]
+  g = hex[3..4]
+  b = hex[5..6]
+  [r, g, b].map { |str| str.hex }
 end
