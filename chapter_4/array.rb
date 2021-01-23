3305e6071c9f82d80dce5811b25f5f74014b27ee
Katsuya Kikuchi <k.kikuchi.ah@gmail.com>
2021-01-24 01:24:56 +0900

    add fizzbuzz file and test

--- chapter_4/array.rb
+++ chapter_4/array.rb
@@ -16,4 +16,32 @@ end
 a.delete_if do |value|
   value.even?
 end
+# 1行でもかける
+a.each { |value| puts value }
+
+a << 14
 puts a
+
+# ブロックの処理の返り値で配列の値を上書き
+a = a.map { |value| value ** 2 }
+puts a
+
+# rust の fold
+sum = a.inject(0) { |s, n| s += n }
+puts sum
+
+range = 3..10
+puts range
+
+str = "hello ruby world!"
+# 範囲を抜け出せる
+puts str[range]
+
+puts "#{x} #{y} #{z}"
+range = x..y
+puts range
+puts str[range]
+
+# 範囲オブジェクトから配列を生成
+arr = ('a'..'bc').to_a
+puts arr
