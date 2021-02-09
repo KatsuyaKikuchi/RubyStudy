f6635e0dcc6f501b415f6c9377ca30111383a5be
Katsuya Kikuchi <k.kikuchi.ah@gmail.com>
2021-02-09 22:28:07 +0900

    study module

---
+++ chapter_8/comparable.rb
@@ -0,0 +1,24 @@
+class Tempo
+  include Comparable
+
+  attr_reader :bpm
+
+  def initialize(bpm)
+    @bpm = bpm
+  end
+
+  def <=>(other)
+    if other.is_a?(Tempo)
+      bpm <=> other.bpm
+    else
+      nil
+    end
+  end
+end
+
+t_120 = Tempo.new(120)
+t_180 = Tempo.new(180)
+
+puts t_120 > t_180
+puts t_120 == t_180
+puts t_120 < t_180
