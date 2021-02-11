c546a54dc66a9bc24e2b9746d4d59969ce38bf70
Katsuya Kikuchi <k.kikuchi.ah@gmail.com>
2021-02-11 23:20:34 +0900

    study module

---
+++ chapter_9/greeter.rb
@@ -0,0 +1,27 @@
+module Greeter
+  def hello
+    'hellor'
+  end
+end
+
+puts 'start'
+begin
+  greeter = Greeter.new
+rescue StandardError => e
+  # 例外クラスの捕捉
+  puts e.class
+  puts e.message
+  puts e.backtrace
+end
+count = 3
+begin
+  0 / 0
+rescue StandardError
+  if count >= 0
+    puts 'retry'
+    count -= 1
+    retry
+  end
+end
+
+puts 'end'
